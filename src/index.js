81b616ff5d54195e5ae78a9c3ca08bdcf7892dd5
Martin Obadal <4430316+TheKvikk@users.noreply.github.com>
2025-06-03 15:08:36 +0200

    init

---
+++ src/index.js
@@ -0,0 +1,8 @@
+window.addEventListener('scroll', () => {
+	const header = document.querySelector('.header');
+	if (window.scrollY > 0) {
+		header.classList.add('header--scrolled');
+	} else {
+		header.classList.remove('header--scrolled');
+	}
+});

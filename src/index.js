5613aa0aec1c398a54a63a5a7b0ddf9968619723
Martin Obadal <4430316+TheKvikk@users.noreply.github.com>
2025-06-18 21:17:04 +0200

    update

--- src/index.js
+++ src/index.js
@@ -15,3 +15,53 @@ window.addEventListener('scroll', () => {
 		scrollTopButton.style.opacity = 0;
 	}
 });
+
+window.dispatchEvent(new Event('scroll'));
+
+document.getElementById('scroll-top').addEventListener('click', () => {
+	window.scroll({
+		top: 0,
+		behavior: "smooth"
+	});
+});
+
+document.getElementById('open-nav').addEventListener('click', () => {
+	document.querySelector("nav").classList.add("opened");
+});
+
+document.getElementById('close-nav').addEventListener('click', () => {
+	document.querySelector('nav').classList.remove('opened');
+});
+
+const items = document.querySelectorAll('.accordion');
+
+let openItem = null;
+
+items.forEach(item => {
+	const header = item.querySelector('.accordion__title');
+	const content = item.querySelector('.accordion__content');
+
+	header.addEventListener('click', () => {
+		const isOpen = !content.classList.contains('accordion__content--hidden');
+
+		if (openItem && openItem !== item) {
+			toggleItem(openItem, false);
+		}
+
+		toggleItem(item, !isOpen);
+
+		openItem = !isOpen ? item : null;
+	});
+});
+
+function toggleItem(item, toOpen) {
+	const content = item.querySelector('.accordion__content');
+
+	if (toOpen) {
+		content.classList.remove('accordion__content--hidden');
+		content.style.maxHeight = `${content.scrollHeight}px`;
+	} else {
+		content.classList.add('accordion__content--hidden');
+		content.style.maxHeight = '0';
+	}
+}
